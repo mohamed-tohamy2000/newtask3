5eb218be1d1b878c7d8f642de2d67b4c35297015
mando-1993 <workm1479@gmail.com>
2025-09-17 21:48:36 +0300

    frist24

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 
 
 export default function Header() {
@@ -8,8 +7,8 @@ export default function Header() {
         <h1>Logo</h1>
 
         <div className="flex gap-10">
-          <Link to="/">Home</Link>
-          <Link to="/posts">Posts</Link>
+          <a href="/">Home</a>
+          <a href="/posts">Posts</a>
         </div>
 
         <button className="btn btn-dash rounded-2xl">Booking</button>
